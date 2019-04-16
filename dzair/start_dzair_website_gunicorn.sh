877b525d0c4bc936d0da1a271e2710bf0c8799d6
Sofiane <sofiane.abbar@gmail.com>
2019-04-17 00:42:28 +0300

    changed style of the map

--- dzair/start_dzair_website_gunicorn.sh
+++ dzair/start_dzair_website_gunicorn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn --workers=2 --bind=0.0.0.0:5001 --log-level=debug qarta:app --access-logfile=logs/access.log --error-logfile=logs/error.log
+gunicorn --workers=2 --bind=0.0.0.0:5001 --log-level=debug dzair:app --access-logfile=logs/access.log --error-logfile=logs/error.log
