ec8c50ca03daf978fbab255804640cfa3bdb8c60
Sofiane <sofiane.abbar@gmail.com>
2019-04-17 17:19:47 +0300

    added static count info

--- dzair/data_preparation/to_csv.py
+++ dzair/data_preparation/to_csv.py
@@ -8,8 +8,7 @@ ID_2: commune
 """
 
 x = json.load(open('communes.geojson'))
-for _ in x['features']:
-	p = _['properties']
-	print u'' + p['NAME_1'].encode('utf8') + u','+p['NAME_2'].encode('utf8') + u','+ str(p['ID_2']) 
-	#print u'%s,%s,%s\n' % (p['NAME_1'] ,p['NAME_2'] ,p['ID_2']) 
-	# print p['NAME_1'],',',p['NAME_2'],',', p['ID_2'] 
+with open('communes.csv', 'w') as g:
+	for _ in x['features']:
+		p = _['properties']
+		g.write('%s,%s,%s\n' % (p['ID_2'], p['NAME_1'].encode('utf8') ,p['NAME_2'].encode('utf8'))) 
