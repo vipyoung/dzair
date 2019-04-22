22388edd75fa914be0d3d372deceee2b36aa405d
Sofiane <sofiane.abbar@gmail.com>
2019-04-22 06:10:16 +0300

    added wilaya level

--- dzair/data_preparation/update_vote.py
+++ dzair/data_preparation/update_vote.py
@@ -1,5 +1,8 @@
 import json
 import sys
+from collections import defaultdict
+
+
 """
 NAME_1: wilaya
 NAME_2: commune
@@ -7,14 +10,43 @@ ID_1: wilaya
 ID_2: commune
 """
 
-ids_str = sys.argv[1]
-ids = map(int, ids_str.split(','))
+ids_file = sys.argv[1]
+output_file = sys.argv[2]
+output_file_wilaya = sys.argv[3]
 
-with open('communes_vote.geojson') as f:
+ids = map(int, open(ids_file).read().split('\n')[:-1])
+print len(ids), ids
+
+features = []
+w_ids = defaultdict(int)
+with open('communes.geojson') as f:
 	x = json.load(f)
 	for _ in x['features']:
 		p = _['properties']
 		if p['ID_2'] in ids:
 			p['VOTE'] = 0
+			features.append(_)
+			w_ids[p['ID_1']] += 1
+x['features'] = features
 
-json.dump(x, open('communes_vote.geojson', 'w'))
+# Write commune js
+with open(output_file, 'w') as g:
+	g.write('var commData = %s;' % json.dumps(x))
+
+# write wilaya js
+
+features = []
+with open('wilayas.geojson') as f:
+	x = json.load(f)
+	for _ in x['features']:
+		p = _['properties']
+		if p['ID_1'] in w_ids:
+			p['VOTE'] = 0
+			p['NAME_2'] = w_ids[p['ID_1']]
+			features.append(_)
+
+x['features'] = features
+
+with open(output_file_wilaya, 'w') as g:
+	g.write('var wilaData = %s;' % json.dumps(x))
+# json.dump(x, open('communes_vote.geojson', 'w'))
